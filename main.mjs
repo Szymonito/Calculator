c57e2cd1cf3a4093dd26b65c43a54189387a4eb5
Szymonito <Szymon.Nowakowski@cyfroweszkoly.pl>
2024-11-02 12:54:44 +0100

    funkcja main, metoda callback

--- main.mjs
+++ main.mjs
@@ -29,3 +29,35 @@ import readline from 'readline'
             }
         }, 1000)
     }
+
+    async function main(){
+        const a = parseInt (await askUser("Podaj pierwsza liczbe"))
+        const b = parseInt (await askUser("Podaj druga liczbe"))
+        const operation = await askUser("Chcesz wykonac dodawanie czy mnozenie?")
+        const method = await askUser("chcesz uzyc metody callback czy moze promise?")
+
+        if(method == "callback"){
+            asyncOperation(a, b, operation, (error, result) => {
+                if(error) {
+                    console.error(error)
+                }
+                else {
+                    console.log (`Wynik (callback) ${result}`)
+                }
+            })
+            rl.close()
+        }
+
+        else if (method == "promise") {
+            promiseOperation(a, b, operation)
+                .then(result => console.log(`Wynik (promise) ${result}`))
+            
+        }
+
+
+
+
+
+
+
+    }
