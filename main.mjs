52c0a87cc73aa6dca9e2a7f175079caaa39a4a11
Szymonito <Szymon.Nowakowski@cyfroweszkoly.pl>
2024-11-02 12:45:41 +0100

    operacje

--- main.mjs
+++ main.mjs
@@ -4,3 +4,28 @@ import readline from 'readline'
         input: process.stdin,
         output: process.stdout
     })
+
+    function askUser(question) {
+        return new Promise (resolve => {
+            rl.question(question, answer => {
+                resolve (answer)
+            })
+        })
+    }
+
+    function asyncOperation (a, b, operation, callback) {
+        setTimeout(() => {
+            if(typeof a !== 'number' || typeof b !== "number" || (operation !== "dodawanie" && operation !== "mnozenie")){
+                callback(new Error("Dane zostały zle wprowadzone"), null)
+                return
+            }
+
+            if (operation == "dodawanie"){
+                callback(null, a + b)
+            }
+
+            if(operation == "mnozenie"){
+                callback(null, a * b)
+            }
+        }, 1000)
+    }
